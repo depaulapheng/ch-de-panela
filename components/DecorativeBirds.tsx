48b55dae61e6ffaaeef1d07935ce1d95524f7394
depaulapheng <depaulaph.eng@gmail.com>
2026-09-18 16:42:34 -0300

    feat: add decorative pink birds

---
+++ components/DecorativeBirds.tsx
@@ -0,0 +1,25 @@
+export function DecorativeBirds(){
+  return <div className="bird-scene" aria-hidden="true">
+    <svg className="bird bird-one" viewBox="0 0 180 120" role="presentation">
+      <g fill="currentColor">
+        <path d="M70 63c-8-14-24-26-49-32 19 14 26 29 26 43-16-4-29-2-40 6 20 1 35 9 46 22 9 10 19 16 31 18-4-10-5-20-2-31 8 5 17 7 27 6-10-5-17-12-22-21 14 2 28-1 42-9-20-2-38-10-59-2Z"/>
+        <path d="M93 59c13-13 28-23 48-29-12 10-19 20-23 31 18-3 35 1 50 10-21-1-38 5-50 17-9 8-20 13-31 13 5-12 7-26 6-42Z"/>
+        <circle cx="89" cy="58" r="9"/>
+        <path d="M95 58 129 48 101 65Z"/>
+        <path d="M80 72 59 103 88 84Z"/>
+      </g>
+    </svg>
+    <svg className="bird bird-two" viewBox="0 0 180 120" role="presentation">
+      <g fill="currentColor">
+        <path d="M70 63c-8-14-24-26-49-32 19 14 26 29 26 43-16-4-29-2-40 6 20 1 35 9 46 22 9 10 19 16 31 18-4-10-5-20-2-31 8 5 17 7 27 6-10-5-17-12-22-21 14 2 28-1 42-9-20-2-38-10-59-2Z"/>
+        <path d="M93 59c13-13 28-23 48-29-12 10-19 20-23 31 18-3 35 1 50 10-21-1-38 5-50 17-9 8-20 13-31 13 5-12 7-26 6-42Z"/>
+        <circle cx="89" cy="58" r="9"/>
+        <path d="M95 58 129 48 101 65Z"/>
+        <path d="M80 72 59 103 88 84Z"/>
+      </g>
+    </svg>
+    <span className="petal petal-one"/>
+    <span className="petal petal-two"/>
+    <span className="petal petal-three"/>
+  </div>;
+}
